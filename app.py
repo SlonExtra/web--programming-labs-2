88619461e7033355f9924bbbd973c9276db24bc6
Дмитрий Руди <drbasket2004@gmail.com>
2024-09-26 21:32:29 +0700

    меню

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__) 
 
 @app.route('/')
+@app.route('/index')
 
 def start():
     return """
@@ -24,6 +25,37 @@ def start():
                 </li>
 
             </ol>
+        </div>
+
+        <footer>
+            &copy; Руди Дмитрий, ФБИ-24, 3 курс, 2024 
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route('/lab1')
+def lab1():
+     return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Руди Дмитрий Константинович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <div>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые ба-
+            зовые возможности.
         </div>
 
         <footer>
