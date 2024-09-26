58f3cfc57791b86e9c4ff8b8a6ff95e886cddf8d
Дмитрий Руди <drbasket2004@gmail.com>
2024-09-26 21:22:01 +0700

    вывод страницы лабораторных

--- app.py
+++ app.py
@@ -4,4 +4,31 @@ app = Flask(__name__)
 @app.route('/')
 
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Руди Дмитрий Константинович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <div>
+            <ol>
+                <li>
+                    <a href="/lab1">Лабораторная работа 1</a>
+                </li>
+
+            </ol>
+        </div>
+
+        <footer>
+            &copy; Руди Дмитрий, ФБИ-24, 3 курс, 2024 
+        </footer>
+    </body>
+</html>
+"""
